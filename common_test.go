8c9348b372cb5619c8fd8bb8fb87ae5d81428280
Ho Trong Duc <ducmeit2015@gmail.com>
2019-08-22 00:44:58 +0700

    Update code

--- common_test.go
+++ common_test.go
@@ -2,9 +2,21 @@ package algorithm
 
 import "testing"
 
+func Test_Two_Sum(t *testing.T) {
+	nums := []int{2,7,14,19}
+	target := 9
+	expected := []int{0, 1}
+	result := twoSum(nums, target)
+	for i := 0; i < len(expected); i++ {
+		if expected[i] != result[i] {
+			t.Fatalf("Expected: %v  | Actual: %v", expected, result)
+		}
+	}
+}
+
 func Test_Reverse_Integer(t *testing.T) {
-	result := reverse(123)
-	expected := 321
+	result := reverse(-987)
+	expected := -789
 	if result != expected {
 		t.Fatalf("Expected: %d | Actual: %d", expected, result)
 	}
--- reverse_integer.go
+++ reverse_integer.go
@@ -1,1 +1,29 @@
 package algorithm
+
+import (
+	"math"
+)
+
+func reverseNumber(x int) int {
+	result := 0
+	for x > 0 {
+		ld := x % 10
+		result = result * 10 + ld
+		x = x/10
+	}
+
+	if result < math.MinInt32 || result > math.MaxInt32 {
+		return 0
+	}
+
+	return result
+}
+
+func reverse(x int) int {
+	signed := 1
+	if x < 0 {
+		signed = -1
+	}
+	x = int(math.Abs(float64(x)))
+	return reverseNumber(x) * signed
+}
