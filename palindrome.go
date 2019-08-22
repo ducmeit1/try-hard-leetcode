8e51d41e1bd0bb07aae540ce9a7869baecd8b76d
Đức Trọng <ducmeit2015@gmail.com>
2019-08-23 00:44:40 +0700

    Give best solution for palindrome

--- palindrome.go
+++ palindrome.go
@@ -3,33 +3,19 @@ package algorithm
 import "math"
 
 func isPalindrome(x int) bool {
+	if x < 0 || (x % 10 == 0 && x > 0) {
+		return false
+	}
+
 	arr := make([]int, 0)
-	sign := getSign(x)
-	tmp := x
 	x = int(math.Abs(float64(x)))
 	for x > 0 {
 		ld := x % 10
-		if x * sign == tmp {
-			ld = ld * sign
-		}
 		arr = append(arr, ld)
 		x = x / 10
 	}
 
 	length := len(arr)
-	if length == 1 {
-		if arr[0] >= 0 {
-			return true
-		}
-		return false
-	}
-
-	if length == 2 {
-		if arr[0] != arr[1] {
-			return false
-		}
-		return true
-	}
 
 	mid := length / 2
 	s1 := arr[0:mid]
@@ -45,9 +31,4 @@ func isPalindrome(x int) bool {
 	}
 
 	return true
-}
-
-func getSign(x int) int {
-	if x > 0 {return 1}
-	return -1
 }
